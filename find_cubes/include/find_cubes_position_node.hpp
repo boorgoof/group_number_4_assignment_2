88071c45112f2f78257dd705e70939d7c9d76f5b
boorgoof <federico.31mene@icloud.com>
2025-12-23 14:57:35 +0100

    published cubes position in an array

--- find_cubes/include/find_cubes_position_node.hpp
+++ find_cubes/include/find_cubes_position_node.hpp
@@ -13,6 +13,8 @@
 
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
+#include "find_cubes/msg/cubes_poses.hpp"
+
 
 class FindCubesPositionNode : public rclcpp::Node
 {
@@ -21,37 +23,35 @@ public:
 
 private:
 
-  // Subscriber callback
-  void cubes_pos_callback(const apriltag_msgs::msg::AprilTagDetectionArray::SharedPtr msg);
+    // Subscriber callback
+    void cubes_pos_callback(const apriltag_msgs::msg::AprilTagDetectionArray::SharedPtr msg);
 
-  // helpers
-  bool apriltag_pose_wrt_odom(const std::string & tag_frame,const rclcpp::Time & stamp, geometry_msgs::msg::PoseStamped & out_pose);
-  bool has_pose_changed(const geometry_msgs::msg::PoseStamped & prev_pose, const geometry_msgs::msg::PoseStamped & new_pose);
+    // helpers
+    bool get_pose_in_frame(const std::string & target_frame, const std::string & source_frame, const rclcpp::Time & stamp, geometry_msgs::msg::PoseStamped & out_pose);
+    bool poses_changed( const find_cubes::msg::CubesPoses & prev, const find_cubes::msg::CubesPoses & now_msg) const;
+    
+    // Subscriber
+    rclcpp::Subscription<apriltag_msgs::msg::AprilTagDetectionArray>::SharedPtr detections_sub_;
 
-  // Subscriber
-  rclcpp::Subscription<apriltag_msgs::msg::AprilTagDetectionArray>::SharedPtr detections_sub_;
+    // TF2
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
-  // TF2
-  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    // Publisher
+    rclcpp::Publisher<find_cubes::msg::CubesPoses>::SharedPtr cubes_pub_;
 
-  // Publishers 
-  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr red_pose_publisher_;
-  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr blue_pose_publisher_;
 
-  // Frames 
-  std::string tag_frame_prefix_ = "tag36h11:";
-  std::string odom_frame_ = "odom";
+    // Frames 
+    std::string tag_frame_prefix_ = "tag36h11:";
+    std::string base_link_frame_ = "base_link";
 
-  // Previous poses
-  geometry_msgs::msg::PoseStamped previous_red_cube_pose_;
-  geometry_msgs::msg::PoseStamped previous_blue_cube_pose_;
-  bool has_previous_pose_red_cube_ = false;
-  bool has_previous_pose_blue_cube_ = false;
+    // Previous poses
+    find_cubes::msg::CubesPoses previous_;
+    bool has_previous_ = false;
 
-  // Thresholds
-  double position_threshold_ = 0.01;
-  double orientation_threshold_ = 0.05;
+    // Thresholds
+    double position_threshold_ = 0.01;
+    double orientation_threshold_ = 0.05;
 };
 
 #endif  // FIND_CUBES_POSITION_NODE_HPP_
--- find_cubes/src/find_cubes_position_node.cpp
+++ find_cubes/src/find_cubes_position_node.cpp
@@ -9,15 +9,11 @@ FindCubesPositionNode::FindCubesPositionNode() : Node("find_cubes_position_node"
     tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-    tag_frame_prefix_ = "tag36h11:";
-    odom_frame_ = "odom";
-
     auto qos = rclcpp::QoS(rclcpp::KeepLast(1));
     qos.transient_local();
     qos.reliable();
 
-    red_pose_publisher_  = this->create_publisher<geometry_msgs::msg::PoseStamped>("/red_cube_pose", qos);
-    blue_pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("/blue_cube_pose", qos);
+    cubes_pub_ = this->create_publisher<find_cubes::msg::CubesPoses>("/cubes_poses", qos);
 
     detections_sub_ = this->create_subscription<apriltag_msgs::msg::AprilTagDetectionArray>(
         "/apriltag/detections",
@@ -27,8 +23,7 @@ FindCubesPositionNode::FindCubesPositionNode() : Node("find_cubes_position_node"
             this->cubes_pos_callback(msg);
         });
 
-    has_previous_pose_red_cube_ = false;
-    has_previous_pose_blue_cube_ = false;
+    has_previous_ = false;
     position_threshold_ = 0.01;
     orientation_threshold_ = 0.05;
 }
@@ -36,86 +31,105 @@ FindCubesPositionNode::FindCubesPositionNode() : Node("find_cubes_position_node"
 
 void FindCubesPositionNode::cubes_pos_callback(const apriltag_msgs::msg::AprilTagDetectionArray::SharedPtr msg)
 {
-    if (msg->detections.size() < 2) {
-    RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 2000,
-        "Apriltags detected: %zu (need at least 2)", msg->detections.size());
-    return;
+    if (msg->detections.size() != 2) {
+        RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 2000,
+        "Apriltags detected: %zu (we want = 2)", msg->detections.size());
+        return;
     }
 
     const int id0 = msg->detections[0].id;
     const int id1 = msg->detections[1].id;
 
-    // we expect to see both tags: id=1 (red cube) and id=10 (blue cube)
     if (!((id0 == 1 && id1 == 10) || (id0 == 10 && id1 == 1))) {
-    RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 2000,
+        RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 2000,
         "Unexpected tag IDs: [%d, %d]. Expected {1,10}", id0, id1);
         return;
     }
 
-    // red_id=1, blue_id=10
-    const int red_id  = 1;
-    const int blue_id = 10;
-
-    const std::string frame_red  = tag_frame_prefix_ + std::to_string(red_id);
-    const std::string frame_blue = tag_frame_prefix_ + std::to_string(blue_id);
-
     geometry_msgs::msg::PoseStamped red_pose;
     geometry_msgs::msg::PoseStamped blue_pose;
 
-    if (!apriltag_pose_wrt_odom(frame_red, msg->header.stamp, red_pose)) {
-        RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 2000,
-            "Failed TF lookup for RED frame %s", frame_red.c_str());
+    //todo modify with colors
+    const std::string frame_red  = tag_frame_prefix_ + "1";
+    const std::string frame_blue = tag_frame_prefix_ + "10";
+
+    if (!get_pose_in_frame(base_link_frame_, frame_red, msg->header.stamp, red_pose)) {
+        RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 2000,
+        "TF lookup failed: %s -> %s", base_link_frame_.c_str(), frame_red.c_str());
+        return;
+    }
+    if (!get_pose_in_frame(base_link_frame_, frame_blue, msg->header.stamp, blue_pose)) {
+        RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 2000,
+        "TF lookup failed: %s -> %s", base_link_frame_.c_str(), frame_blue.c_str());
         return;
     }
 
-    if (!apriltag_pose_wrt_odom(frame_blue, msg->header.stamp, blue_pose)) {
-        RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 2000,
-            "Failed TF lookup for BLUE frame %s", frame_blue.c_str());
-        return;
-    }
+    find_cubes::msg::CubesPoses out;
+    out.header.stamp = now();
+    out.header.frame_id = base_link_frame_;
 
-    
-    if (!has_previous_pose_red_cube_ || has_pose_changed(previous_red_cube_pose_, red_pose)) {
-        red_pose_publisher_->publish(red_pose);
-        previous_red_cube_pose_ = red_pose;
-        has_previous_pose_red_cube_ = true;
-    }
+    out.ids = {1, 10};
+    out.poses.clear();
+    out.poses.push_back(red_pose.pose);
+    out.poses.push_back(blue_pose.pose);
 
-    if (!has_previous_pose_blue_cube_ || has_pose_changed(previous_blue_cube_pose_, blue_pose)) {
-        blue_pose_publisher_->publish(blue_pose);
-        previous_blue_cube_pose_ = blue_pose;
-        has_previous_pose_blue_cube_ = true;
+    if (!has_previous_ || poses_changed(previous_, out)) {
+        cubes_pub_->publish(out);
+        previous_ = out;
+        has_previous_ = true;
+
+        RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000,
+            "Published /cubes_poses: red(%.3f,%.3f) blue(%.3f,%.3f)",
+            out.poses[0].position.x, out.poses[0].position.y,
+            out.poses[1].position.x, out.poses[1].position.y);
     }
 }
 
-bool FindCubesPositionNode::apriltag_pose_wrt_odom(const std::string & tag_frame, const rclcpp::Time & stamp, geometry_msgs::msg::PoseStamped & out_pose)
+bool FindCubesPositionNode::get_pose_in_frame(const std::string & target_frame, const std::string & source_frame, const rclcpp::Time & stamp, geometry_msgs::msg::PoseStamped & out_pose)
 {
-  geometry_msgs::msg::TransformStamped tf_tag;
-  try {
-    tf_tag = tf_buffer_->lookupTransform(odom_frame_, tag_frame, stamp, tf2::durationFromSec(0.1));
-  } catch (const tf2::TransformException &) {
+    geometry_msgs::msg::TransformStamped tf;
+    try {
+        tf = tf_buffer_->lookupTransform(
+            target_frame,
+            source_frame,
+            stamp,
+            tf2::durationFromSec(0.1));
+    } catch (const tf2::TransformException &) {
+        return false;
+    }
+
+    out_pose.header.stamp = stamp;
+    out_pose.header.frame_id = target_frame;
+    out_pose.pose.position.x = tf.transform.translation.x;
+    out_pose.pose.position.y = tf.transform.translation.y;
+    out_pose.pose.position.z = tf.transform.translation.z;
+    out_pose.pose.orientation = tf.transform.rotation;
+
+    return true;
+}
+
+bool FindCubesPositionNode::poses_changed(const find_cubes::msg::CubesPoses & prev, const find_cubes::msg::CubesPoses & now_msg) const
+{
+    if (prev.poses.size() != now_msg.poses.size())
+        return true;
+
+    for (size_t i = 0; i < prev.poses.size(); ++i)
+    {
+        const auto & a = prev.poses[i];
+        const auto & b = now_msg.poses[i];
+
+        const double dx = a.position.x - b.position.x;
+        const double dy = a.position.y - b.position.y;
+        const double dz = a.position.z - b.position.z;
+        const double pos = std::sqrt(dx * dx + dy * dy + dz * dz);
+
+        const double oz = std::abs(a.orientation.z - b.orientation.z);
+        const double ow = std::abs(a.orientation.w - b.orientation.w);
+        const double orient = oz + ow;
+
+        if (pos > position_threshold_ || orient > orientation_threshold_)
+            return true;
+    }
+
     return false;
-  }
-
-  out_pose.header.stamp = this->now();
-  out_pose.header.frame_id = odom_frame_;
-  out_pose.pose.position.x = tf_tag.transform.translation.x;
-  out_pose.pose.position.y = tf_tag.transform.translation.y;
-  out_pose.pose.position.z = tf_tag.transform.translation.z;
-  out_pose.pose.orientation = tf_tag.transform.rotation;
-  return true;
-}
-
-bool FindCubesPositionNode::has_pose_changed(const geometry_msgs::msg::PoseStamped & prev, const geometry_msgs::msg::PoseStamped & now)
-{
-  const double dx = now.pose.position.x - prev.pose.position.x;
-  const double dy = now.pose.position.y - prev.pose.position.y;
-  const double dz = now.pose.position.z - prev.pose.position.z;
-  const double pos_diff = std::sqrt(dx*dx + dy*dy + dz*dz);
-
-  const double oz = std::abs(now.pose.orientation.z - prev.pose.orientation.z);
-  const double ow = std::abs(now.pose.orientation.w - prev.pose.orientation.w);
-  const double orient_diff = oz + ow;
-
-  return (pos_diff > position_threshold_) || (orient_diff > orientation_threshold_);
 }
