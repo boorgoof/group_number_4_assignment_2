0f7f1c363a10795716fb4fad9146989b16b27e88
endlessDoomsayer <matteo.bino@studenti.unipd.it>
2025-12-27 12:21:03 +0100

    fixed logic controller variables, added 20 seconds delay to its start to let everything set up correctly

--- logic_controller/include/logic_controller/behaviour_tree.hpp
+++ logic_controller/include/logic_controller/behaviour_tree.hpp
@@ -11,6 +11,28 @@
 #include "motion_controller/action/go_home.hpp"
 #include "find_cubes/msg/cubes_poses.hpp"
 
+namespace BT
+{
+    template<>
+    inline geometry_msgs::msg::Pose convertFromString(StringView str)
+    {
+        // Expected format: "x;y;z;qx;qy;qz;qw"
+        auto parts = splitString(str, ';');
+        if (parts.size() != 7) {
+            throw RuntimeError("Invalid Pose format. Expected 7 values separated by ';'");
+        }
+        geometry_msgs::msg::Pose pose;
+        pose.position.x = convertFromString<double>(parts[0]);
+        pose.position.y = convertFromString<double>(parts[1]);
+        pose.position.z = convertFromString<double>(parts[2]);
+        pose.orientation.x = convertFromString<double>(parts[3]);
+        pose.orientation.y = convertFromString<double>(parts[4]);
+        pose.orientation.z = convertFromString<double>(parts[5]);
+        pose.orientation.w = convertFromString<double>(parts[6]);
+        return pose;
+    }
+}
+
 using namespace BT;
 
 class MoveCubeAction : public StatefulActionNode {
@@ -25,7 +47,8 @@ public:
             InputPort<geometry_msgs::msg::Pose>("from"),
             InputPort<geometry_msgs::msg::Pose>("to"),
             InputPort<bool>("use_waypoint"),
-            InputPort<geometry_msgs::msg::Pose>("waypoint_pose")
+            InputPort<geometry_msgs::msg::Pose>("waypoint_pose"),
+            InputPort<float>("waypoint_wait_time")
         };
     }
 
@@ -33,10 +56,21 @@ public:
         if (!client_->wait_for_action_server(std::chrono::seconds(2))) return NodeStatus::FAILURE;
 
         auto goal = motion_controller::action::MoveCube::Goal();
+
+        if (!getInput("from", goal.pose_from) || !getInput("to", goal.pose_to)) {
+            return NodeStatus::FAILURE;
+        }
+        
+        goal.use_waypoint = false;
         getInput("from", goal.pose_from);
         getInput("to", goal.pose_to);
         getInput("use_waypoint", goal.use_waypoint);
-        getInput("waypoint_pose", goal.waypoint_pose);
+
+        if (goal.use_waypoint){
+            getInput("waypoint_pose", goal.waypoint_pose);
+            getInput("waypoint_wait_time", goal.waypoint_wait_time);
+        }
+        
 
         auto send_goal_options = rclcpp_action::Client<motion_controller::action::MoveCube>::SendGoalOptions();
         send_goal_options.result_callback = [this](const auto& result) { 
@@ -109,17 +143,19 @@ public:
     }
 
     static PortsList providedPorts() {
-        return { OutputPort<geometry_msgs::msg::Pose>("red_pose"), OutputPort<geometry_msgs::msg::Pose>("blue_pose") };
+        return { OutputPort<geometry_msgs::msg::Pose>("cube1_pose"), OutputPort<geometry_msgs::msg::Pose>("cube2_pose") };
     }
 
     NodeStatus tick() override {
         if (!last_msg_) return NodeStatus::FAILURE;
         for (size_t i = 0; i < last_msg_->ids.size(); ++i) {
-            if (last_msg_->ids[i] == 1) setOutput("red_pose", last_msg_->poses[i]);
-            if (last_msg_->ids[i] == 10) setOutput("blue_pose", last_msg_->poses[i]);
+            if (last_msg_->ids[i] == id_cube1) setOutput("cube1_pose", last_msg_->poses[i]);
+            if (last_msg_->ids[i] == id_cube2) setOutput("cube2_pose", last_msg_->poses[i]);
         }
         return NodeStatus::SUCCESS;
     }
+
+    static constexpr int id_cube1 = 1, id_cube2 = 10;
 private:
     rclcpp::Subscription<find_cubes::msg::CubesPoses>::SharedPtr sub_;
     find_cubes::msg::CubesPoses::SharedPtr last_msg_;
--- logic_controller/src/robot_controller.cpp
+++ logic_controller/src/robot_controller.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char ** argv) {
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("robot_controller_bt");
-
+    RCLCPP_INFO(node->get_logger(), "\n\n\n\n\n\n\n\n\ncreated!");
     BT::BehaviorTreeFactory factory;
 
     factory.registerBuilder<MoveCubeAction>("MoveCube", 
@@ -24,18 +24,18 @@ int main(int argc, char ** argv) {
             return std::make_unique<GetCubesPoses>(name, config, node);
         });
 
-    std::string pkg_share = ament_index_cpp::get_package_share_directory("robot_controller");
+    std::string pkg_share = ament_index_cpp::get_package_share_directory("logic_controller");
     auto tree = factory.createTreeFromFile(pkg_share + "/config/swap_cubes.xml");
 
     rclcpp::Rate rate(10);
     BT::NodeStatus status = BT::NodeStatus::RUNNING;
-
+    RCLCPP_INFO(node->get_logger(), "running!");
     while (rclcpp::ok() && status == BT::NodeStatus::RUNNING) {
         status = tree.tickOnce();
         rclcpp::spin_some(node);
         rate.sleep();
     }
-
+    RCLCPP_INFO(node->get_logger(), "shutting down!");
     rclcpp::shutdown();
     return 0;
 }
