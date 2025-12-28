fa7e1bae4b9ed5ba98aa2a67a012534bf75e3ddf
endlessDoomsayer <matt.bno.tv@gmail.com>
2025-12-28 14:41:34 +0100

    modified motion launch to use sim time

--- motion_controller/launch/motion_controller.launch.py
+++ motion_controller/launch/motion_controller.launch.py
@@ -1,4 +1,6 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 
@@ -6,15 +8,26 @@ from moveit_configs_utils import MoveItConfigsBuilder
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("ir_gripper", package_name="ir_movit_config").to_moveit_configs()
 
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
     motion_controller_node = Node(
         package="motion_controller",
         executable="robot_manipulator_node",
         name="robot_manipulator",
         parameters=[
-            {"use_sim_time": True},
+            {"use_sim_time": use_sim_time},
             moveit_config.to_dict(),
         ],
         output="screen",
     )
 
-    return LaunchDescription([motion_controller_node])
+    return LaunchDescription([
+        use_sim_time_arg,
+        motion_controller_node
+    ])
